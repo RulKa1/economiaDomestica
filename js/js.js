78e7ba8cedfb0328863ba98db103a3b156241f82
SergiioCP <rrulka41@gmail.com>
2023-11-14 12:31:07 +0100

    [ADD] Estilos en divAvisos

--- js/js.js
+++ js/js.js
@@ -1,13 +1,15 @@
-//Raul Calvo Hdez
 var arrayGastos = [];
 var tiposDeGastos = [];
 var gasto = 0;
+var ultimoConcepto = "";
+
+
 
 function creandoArrayGastos() {
   let gastos = Array.from(document.getElementsByTagName("img"));
   gastos.forEach((element) => {
     arrayGastos.push(element.alt);
-    tiposDeGastos.push(0);  
+    tiposDeGastos.push(0);
   });
   arrayGastos.sort();
   arrayGastos.reverse();
@@ -22,65 +24,65 @@ function peticionGetJson(nombreConcepto, precio) {
   fetch(url, options)
     .then(response => response.json())
     .then(data => {
-      console.log('Precio recibido:', precio); // Muestra el precio recibido del servidor en la consola
+      console.log('Precio recibido:', precio); 
     })
     .catch(error => {
-      console.error('Error:', error); // Maneja los errores si la solicitud no se completa correctamente
+      console.error('Error:', error); 
     });
 }
-function peticionPostJson(endPoint , precio) {
-   // URL a la que quieres enviar la solicitud POST
-const url = 'http://localhost:3000' + endPoint;
+function peticionPostJson(endPoint, precio) {
+  const url = 'http://localhost:3000' + endPoint;
+
+  const opciones = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(end)
+  };
 
 
+  fetch(url, opciones)
+    .then(response => response.json())
+    .then(data => {
+      console.log('Respuesta del servidor:', data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 
-
-const opciones = {
-  method: 'POST', 
-  headers: {
-    'Content-Type': 'application/json' 
-  },
-  body: JSON.stringify(end) 
-};
-
-
-fetch(url, opciones)
-  .then(response => response.json()) 
-  .then(data => {
-    console.log('Respuesta del servidor:', data); 
-  })
-  .catch(error => {
-    console.error('Error:', error); 
-  });
-  
 }
 function hacerGastos(tipo, precio) {
   var precio = prompt("Ingrese el importe del pago para " + tipo.alt + " en €:");
-  
+
   if (precio !== null && !isNaN(parseFloat(precio)) && parseFloat(precio) > 0) {
     precio = parseFloat(precio);
 
-    // Llamada a la nueva función para mostrar el precio en el div
-    mostrarPrecioEnDiv(precio);
+    mostrarPrecioEnDiv(tipo.alt, precio);
 
-    let creandoGasto = arrayGastos.findIndex((palabra) => palabra === tipo.alt);
-    peticionGetJson(tipo.alt, precio);
-    tiposDeGastos[creandoGasto] += 1;
-    gasto1(precio);
   } else {
     alert("Por favor, ingrese un importe válido mayor a cero.");
   }
   limpiarResultado();
 }
-function mostrarPrecioEnDiv(precio) {
-  // Obtén el div donde quieres mostrar el precio
+
+function mostrarPrecioEnDiv(tipo, precio) {
   const divPrecio = document.getElementById("divAvisos");
 
-  // Agrega el nuevo precio al contenido existente del div
-  divPrecio.innerHTML += "Precio ingresado: " + precio + "€<br>";
+  if (ultimoConcepto) {
+    divPrecio.querySelectorAll(`.${ultimoConcepto}`).forEach(el => {
+      el.classList.remove('resaltado');
+    });
+  }
+
+  const nuevoElemento = document.createElement('span');
+  nuevoElemento.classList.add(tipo.replace(), 'resaltado');
+  nuevoElemento.innerHTML = `${tipo} ${precio}€<br><br>`;
+  divPrecio.appendChild(nuevoElemento);
+
+  ultimoConcepto = tipo.replace();
+
 }
-
-
 function gasto1(gastando) {
   gasto += gastando;
 }
@@ -98,9 +100,12 @@ function reiniciar() {
 }
 
 function mostrarGastos() {
+
+
   let resultado = document.getElementById("resultado");
   let mostrarGastos = "";
   let GastosRealizadas = 0;
+
 
   let indices = Array.from(arrayGastos.keys());
   indices.sort((a, b) => tiposDeGastos[b] - tiposDeGastos[a]);
@@ -110,14 +115,15 @@ function mostrarGastos() {
     let cuota = tiposDeGastos[index];
 
     if (cuota !== 0) {
-      // Updated the format for displaying expenses
-      mostrarGastos += `${element} ---- ${cuota} pago${cuota > 1 ? "s" : ""} ---- ${gasto1(cuota)}€\n`;
+      mostrarGastos += cuota + "  Cuota/s de " + element + "\n";
       GastosRealizadas += cuota;
     }
   });
 
-  // Updated the format for displaying the final and average expenses
-  resultado.innerText = mostrarGastos + "\n" + `Gasto final: ${gasto}€\n` + `Gasto medio: ${gasto / GastosRealizadas}€/concept`;
+
+  resultado.innerText = mostrarGastos + "\n" + "Cantidad Media:" + gasto / GastosRealizadas + "€" + "\n" + "Cantidad Final: " + gasto + " € ";
+
+
 
   reiniciar();
 }
