177b65a8e7253c1bc71b0b8edd2125ea55dfc337
SergiioCP <rrulka41@gmail.com>
2023-11-17 13:50:08 +0100

    [ADD] Falta la ventana emergente

--- js/ecoDomestica.js
+++ js/ecoDomestica.js
@@ -3,8 +3,6 @@ var tiposDeGastos = [];
 var gasto = 0;
 var ultimoConcepto = "";
 var numVecesConcepto = [];
-
-
 function peticionGetJson(precio) {
   const url = 'http://localhost:3000/conceptos';
 
@@ -21,10 +19,8 @@ function peticionGetJson(precio) {
       console.error('Error:', error);
     });
 }
-
 function peticionPostJson(endPoint, precio) {
   const url = 'http://localhost:3000' + endPoint;
-
   const opciones = {
     method: 'POST',
     headers: {
@@ -32,8 +28,6 @@ function peticionPostJson(endPoint, precio) {
     },
     body: JSON.stringify(end)
   };
-
-
   fetch(url, opciones)
     .then(response => response.json())
     .then(data => {
@@ -41,32 +35,26 @@ function peticionPostJson(endPoint, precio) {
     })
     .catch(error => {
       console.error('Error:', error);
-    });
-
-}
-
-async function nuevoTramite(pagos) {
+    });}
+    async function nuevoTramite(pagos) {
   // if (event) {
   //   event.preventDefault();
   // }
 let fechaTramite = prompt("Ingrese la fecha del trámite (formato MM/AAAA):");
   const url = 'http://localhost:3000/registros';
-
-  let tramite = {
-    id: tramites()?.length,
+let tramite = {
+    //  id: tramites()?.length,
     fecha: fechaTramite,
     pagos: await pagos
   };
-
-  const opciones = {
+    const opciones = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(tramite)
   };
-
-  fetch(url, opciones)
+fetch(url, opciones)
     .then(response => {
       if (!response.ok) {
         throw new Error('Error en la solicitud: ' + response.statusText);
@@ -79,9 +67,8 @@ let fechaTramite = prompt("Ingrese la fecha del trámite (formato MM/AAAA):");
     .catch(error => {
       console.error('Error:', error);
     });
-
   }
-async function obtenerIdConcepto(nombreConcepto) {
+  async function obtenerIdConcepto(nombreConcepto) {
   const url = 'http://localhost:3000/conceptos?nombre=' + encodeURIComponent(nombreConcepto);
   try {
       const response = await fetch(url);
@@ -95,7 +82,6 @@ async function obtenerIdConcepto(nombreConcepto) {
       return null;
   }
 }
-
 function creandoArrayGastos() {
   let gastos = Array.from(document.getElementsByTagName("img"));
   gastos.forEach((element) => {
@@ -106,8 +92,6 @@ function creandoArrayGastos() {
   arrayGastos.sort();
   arrayGastos.reverse();
 }
-
-
 function tramites() {
   const url = 'http://localhost:3000/registros';
   let arrayRegistros = [];
@@ -120,7 +104,6 @@ function tramites() {
     .then(response => response.json())
     .then(data => {
       arrayRegistros = data
-
     })
     .catch(error => {
       console.error('Error:', error);
@@ -138,15 +121,11 @@ function creandoArrayGastos() {
   arrayGastos.sort();
   arrayGastos.reverse();
 }
-
 function hacerGastos(tipo, precio) {
   var precio = prompt("Ingrese el importe del pago para " + tipo.alt + " en €:");
-
   if (precio !== null && !isNaN(parseFloat(precio)) && parseFloat(precio) > 0) {
     precio = parseFloat(precio);
-
     mostrarPrecioEnDiv(tipo.alt, precio);
-
     let indice = arrayGastos.indexOf(tipo.alt);
     if (indice !== -1) {
       tiposDeGastos[indice] += precio;
@@ -157,7 +136,6 @@ function hacerGastos(tipo, precio) {
   }
    limpiarResultado();
 }
-
 function mostrarPrecioEnDiv(tipo, precio) {
   const divPrecio = document.getElementById("divAvisos");
   const tipoClase = tipo.replace();
@@ -167,37 +145,28 @@ function mostrarPrecioEnDiv(tipo, precio) {
       el.classList.remove('resaltado');
     });
   }
-
   const nuevoElemento = document.createElement('span');
   nuevoElemento.classList.add(tipoClase);
   nuevoElemento.innerHTML = `${tipo} ${precio}€<br><br>`;
   divPrecio.appendChild(nuevoElemento);
-
   divPrecio.querySelectorAll(`.${tipoClase}`).forEach(el => {
     el.classList.add('resaltado');
-
   });
   ultimoConcepto = tipoClase;
 }
-
-
-
 function gasto1(gastando) {
   gasto += gastando;
 }
-
 function limpiarResultado() {
   let resultado = document.getElementById("resultado");
   resultado.innerText = "";
 }
-
 function reiniciar() {
   gasto = 0;
   arrayGastos = [];
   tiposDeGastos = [];
   creandoArrayGastos();
 }
-
 async function mostrarGastos() {
   let arrayPagos = [];
   let resultado = document.getElementById("resultado");
@@ -207,37 +176,38 @@ async function mostrarGastos() {
   let fechaFormato = fechaActual.toLocaleString('es-ES', { hour12: false });
   mostrarGastosTexto = "Fecha: " + fechaFormato + "\n";
 
-  let indices = Array.from(arrayGastos.keys());
+  // Ordenar los índices por el número de veces que cada concepto ha sido gastado, de mayor a menor
+  let indices = Array.from(arrayGastos.keys()).sort((a, b) => numVecesConcepto[b] - numVecesConcepto[a]);
 
   for (let index of indices) {
     let concepto = arrayGastos[index];
     let gastoPorConcepto = tiposDeGastos[index];
+    let numPagos = numVecesConcepto[index];
 
     if (gastoPorConcepto > 0) {
-        let numPagos = numVecesConcepto[index];
-        let gastoMedio = gastoPorConcepto / numPagos;
-        mostrarGastosTexto += `${concepto} ---- ${numPagos} pagos ---- Gasto Medio de ${concepto}: ${gastoMedio.toFixed(2)}€ ---- Total: ${gastoPorConcepto.toFixed(2)}€\n`;
+      let gastoMedio = gastoPorConcepto / numPagos;
+      mostrarGastosTexto += `${concepto} ---- ${numPagos} pagos ---- Gasto Medio de ${concepto}: ${gastoMedio.toFixed(2)}€ ---- Total: ${gastoPorConcepto.toFixed(2)}€\n`;
 
-        let idDelConcepto = await obtenerIdConcepto(concepto); 
-        arrayPagos.push({
-            idConcepto: idDelConcepto,
-            importeTotal: gastoPorConcepto.toFixed(2),
-            numPagos: numPagos
-        });
+      let idDelConcepto = await obtenerIdConcepto(concepto); 
+      arrayPagos.push({
+        idConcepto: idDelConcepto,
+        importeTotal: gastoPorConcepto.toFixed(2),
+        numPagos: numPagos
+      });
+
+      gastoTotal += gastoPorConcepto;
     }
-}
+  }
 
   let gastoMedioTotal = gastoTotal / indices.length;
   mostrarGastosTexto += `\nGasto final: ${gastoTotal.toFixed(2)}€\nGasto medio por concepto: ${gastoMedioTotal.toFixed(2)}€`;
-
   resultado.innerText = mostrarGastosTexto;
-  
-    nuevoTramite(arrayPagos);
-  
-  //    setTimeout(limpiarResultado, 10000);
-  //  setTimeout(limpiarInformacion, 10000);
-  
+
+   await nuevoTramite(arrayPagos);
+  // setTimeout(limpiarResultado, 10000);
+  // setTimeout(limpiarInformacion, 10000);
 }
+
 
 function limpiarInformacion() {
 
